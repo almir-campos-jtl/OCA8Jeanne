ed9351700cc66fda4d570e33b3c73c9f54cd2b4c
Almir Campos <ac@javasd.com>
2015-11-07 16:35:39 -0800

    Question 17 - Format updated

--- OCA8Jeanne/src/assessmenttest/question17/Question17.java
+++ OCA8Jeanne/src/assessmenttest/question17/Question17.java
@@ -10,7 +10,7 @@ public class Question17
 		//System.out.println(test( i -> {i == 5;} ) );					// Option B //Doesn't compile
 		System.out.println(test( (i) -> i == 5 ) );						// Option C
 		//System.out.println(test( (int i) -> i == 5 ) );				// Option D	//Doesn't compile
-		//System.out.println(test( (int i) -> { return i == 5; } ) );	// Option E	//Doesn't compile
+		//System.out.println(test( (int i) -> { return i == 5; } ) );		// Option E	//Doesn't compile
 		System.out.println(test( (i) -> { return i == 5; } ) );			// Option F
 	}
 	
